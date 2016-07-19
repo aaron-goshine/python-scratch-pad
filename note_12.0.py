6264c2d88d63a1c78074baf201e60b1a0e61c59b
Goshine <greenlig@gmail.com>
2016-07-19 07:17:07 +0100

    feat(Implemented center alignment within the terminal):

--- note_12.0.py
+++ note_12.0.py
@@ -47,3 +47,70 @@ def intToOrdinal (n):
     else:
         return str(n) + "th"
 
+##
+# Generate and display one verse of the Twelve Days of Christmas
+# @param n the verse to generate
+# @return (none)
+#
+def displayVerse (n):
+    print "==========================================="
+    print "On the", intToOrdinal(n), "day of Christmas"
+    print("my true love sent to me: ")
+    if n >= 12:
+        print("Twelve drummers drumming,")
+    if n >= 11:
+        print("Eleven pipers piping,")
+    if n >= 10:
+        print("Ten lords a leaping,")
+    if n >= 9:
+        print("Nine ladies dancing,")
+    if n >= 8:
+        print("Eight maids a milking,")
+    if n >= 7:
+        print("Seven swans a swimming,")
+    if n >= 6:
+        print("Six geese a laying,")
+    if n >= 5:
+        print("Five golden rings,")
+    if n >= 4:
+        print("Four calling birds,")
+    if n >= 3:
+        print("Three French hens,")
+    if n >= 2:
+        print("Two turtle doves,")
+    if n == 1:
+        print "A",
+    else:
+        print "And a",
+        print "Partridge in a tree."
+
+for verse in range(1, 13):
+    displayVerse(verse)
+
+##
+# Center a string of characters within the terminal
+#
+
+WIDTH = 80
+
+##
+# Create a new string that will be centered within
+# a given width
+# @param s the string that will be centered
+# @param width the in which the string will be centered
+# @return a new copy of s that contains the leading spaces needed
+# so that s will appear centered when it is printed
+#
+def center(s, width):
+    # if the string is too long to center,
+    # then original string is return
+    if width < len(s):
+        return s
+    # Compute the number of spaces needed and generate the result
+    spaces = (width - len(s)) // 2
+    result = " " * spaces + s
+
+    return result
+
+print(center("A Famous Story", WIDTH))
+

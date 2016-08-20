1339a2f14f3c3449ee20df9cb0795d1117be6421
Goshine <greenlig@gmail.com>
2016-08-20 13:52:46 +0100

    feat(More recursive excersises): Implemented edit distance calculator

--- recusive_total.py
+++ recusive_total.py
@@ -34,3 +34,36 @@ def isPalindrome (s):
     # palindrome
     return s[0] == s[len(s) - 1] and isPalindrome(s[1 : len(s) - 1])
 
+##
+# Compute and display the edit distance between two strings
+#
+
+## Compute the edit distance between two strings as
+# a count of the minimum number of insert,
+# delete and substitute operations needed to transform on string into the
+# other.
+# @param s the first string
+# @param t the second string
+# @param the edit distance between the strings
+def editDistance(s, t):
+    # If one string is empty, then the edit distance is
+    # one insert operation. for each letter in the other string
+    if len(s) == 0:
+        return len(t)
+    elif len(t) == 0:
+        return len(s)
+    else:
+        cost = 0
+        # If the last characters are not equal, set cost to 1
+        if s [len(s) - 1] != t[len(t) - 1]:
+            cost = 1
+        # Compute three distance
+        d1 = editDistance(s[0: len(s) - 1], t) + 1
+        d2 = editDistance(s, t[0: len(t) - 1]) + 1
+        d3 = editDistance(s[0: len(s) - 1], t[0: len(t) - 1]) + cost
+
+        # Return the minimum distance
+        return min(d1, d2, d3)
+
+
+

47bbb3a3c95b8ae84d3d5c9495c2bf3b136ad953
Aaron Goshine <greenlig@gmail.com>
2017-05-31 06:15:11 +0100

    refactor(workout): reorganise workout folder

--- example/string_capword.py
+++
@@ -1,5 +0,0 @@
-import string
-
-s = 'The quick brown fox jumped over the lazy log.'
-print s
-print string.capwords(s)
--- example/string_maketrans.py
+++
@@ -1,7 +0,0 @@
-import string
-
-leet = string.maketrans('abegiloprstz', '463611092572')
-s = 'The quick brown fox jumped over the lazy dog.'
-
-print s
-print s.translate(leet)
